0947ea2b2fc900662ec916222493f05bd1deb883
Shekhawat J <jitendrashekhawat887@gmail.com>
2022-08-24 16:20:01 +0530

    Shopify clone

---
+++ README.md
@@ -0,0 +1,18 @@
+# Important Link
+
+[LIVE Link](https://rode-ui.netlify.app/)
+
+<br>
+
+# Project Description
+> In this project I have cloned the Rode website (rode.com). It is responsive.
+
+*** 
+<br>
+
+
+# Technologies used
+> HTML
+<br>
+
+> Tailwind CSS
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [],
+  theme: {
+    
+    extend: {
+      colors: {
+        mygreen: '#1C8D73',
+        hevygreen: '#124C3F',
+        lightgreen: '#60bf6b',
+        lightpink: '#FCF6ED',
+        myblack: '#242B2E',
+      }
+    }
+  },
+  plugins: [],
+}
